dbed05dbf57dadf49b346db5c859c511bf2c2c05
Jose <jalopez1423@gmail.com>
2018-12-18 20:52:43 -0800

    remove project from list

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import ProjectItem from "./ProjectItem";
 
 class Projects extends Component {
+    deleteProject(id) {
+        this.props.onDelete(id);
+    }
+
     render() {
         let projectItems;
 
@@ -9,12 +13,11 @@ class Projects extends Component {
             projectItems = this.props.projects.map(project => {
                 // console.log(project);
                 return(
-                    <ProjectItem key={project.title} project={project}/>
+                    <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project={project}/>
                 );
             });
         };
 
-        // console.log(this.props);
         return (
             <div className="Projects">
                 <h3>Latest Projects</h3>
